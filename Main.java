1ee4c1ef741b82c86d5a7e3970d5f17b7c35c833
fam2do <108361807+fam2do@users.noreply.github.com>
2022-06-30 18:09:26 +0300

    home work 2

--- Main.java
+++ Main.java
@@ -1,49 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+       //Begin1◦Дана сторона квадрата a. Найти его периметр P = 4·a.
         //Begin1◦Дана сторона квадрата a. Найти его периметр P = 4·a.
         //Ввод или присвоение исодных данных
         /*int a = 5;
         //Вычисление результата
-        int P = 4 * a;
-        //Вывод результата
-        System.out.println(P);
-         */
-        //Begin2◦
-        //. Дана сторона квадрата a. Найти его площадь S = a**2
-        /*int q = 10;
-        int s = q * q;
-        System.out.println("У квадрата со стороной" + a + " площадь " + s);
-         */
-        //Begin4◦. Дан диаметр окружности d. Найти ее длину L = π·d. В качестве
-        //значения π использовать 3.14).
-        /*int d = 10;
-        final double pi = 3.14;
-        double L = pi * d;
-        System.out.println(" У круга с диаметром " + d + " длина окружности " + L);
-         */
-        //Begin5◦. Дана длина ребра куба a. Найти объем куба V = a
-        //3 и площадь его
-        //поверхности S = 6·2
-        /*int a = 5;
-        int v = a * a * a;
-        int s = 6 * a * a;
-        System.out.println("Объём куба: " + v);
-        System.out.println("Площадь поверхностей куба:"+ s);
-         */
-        //Begin6◦. Даны длины ребер a, b, c прямоугольного параллелепипеда. Найти его объем V = a·b·c и площадь поверхности S = 2·(a·b + b·c + a·c).
-        /*int a = 5;
-        int b = 10;
-        int c = 15;
-        int v = a * b * c;
-        int s = 2*(a*b + b*c + a*c);
-        System.out.println("Объём  длины рёбер:" + v);
-        System.out.println("Площадь поверхности:" + s);
-         */
-        //Begin7◦. Найти длину окружности L и площадь круга S заданного радиуса R:L = 2·π·R, S = π·R 2
-        //Begin8◦
-        //. Даны два числа a и b. Найти их среднее арифметическое: (a + b)/2.
-        /*int a = 10;
-        int b = 15;
+@@ -47,6 +47,71 @@ public static void main(String[] args) {
         int d = 2;
         double c = (a + b) / (double)d;
         System.out.println("Среднее арефметическое a и b:" + c);*/
@@ -103,17 +65,19 @@ public class Main {
         System.out.println("Периметр " + p);*/
         //Begin13◦
         //. Даны два круга с общим центром и радиусами R1 и R2 (R1 > R2). Найти площади этих кругов S1 и S2, а также площадь S3 кольца, внешний радиус которого равен R1, а внутренний радиус равен R2:
-        int R1 = 25;
+        /*int R1 = 25;
         int R2 = 5;
-        final double pi = 3.14;
-        int S1 = pi * (R1 * R1);
-
-
-
-
-
-
-
+        double pi = 3.14;
+        double S1 = pi * (R1 * 2);
+        double S2 = pi * (R2 * 2);
+        double S3 = S1 - S2;
+        System.out.println("Площадь круга S1:" + S1);
+        System.out.println("Площадь круга S2:" + S2);
+        System.out.println("Площаь кольца S3:" + S3);*/
+        
+        
+        
+        
+        
     }
 }
-
