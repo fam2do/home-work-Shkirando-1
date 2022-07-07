646188594687f59a7bfec6215aed3a70293f1c71
fam2do <108361807+fam2do@users.noreply.github.com>
2022-07-07 18:32:15 +0300

    Add files via upload

---
+++ HomeWork2.1.java
@@ -0,0 +1,48 @@
+package lesson.pkg2;
+//Шкирандо Владислав Integer 15-16
+import java.util.Scanner;
+
+/**
+ *
+ * @author famidos
+ */
+public class Lesson2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        //Integer15 Дано трехзначное число. Вывести число
+        //полученное при перестановке цифр сотен и десятков исходного числа (например, 123 перейдет 213).
+
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите трёхзначное число: ");
+        int number = in.nextInt();
+        int one = number / 100;
+        int two = (number / 10)%10;
+        int free = number % 10;
+        int four = two * 100 + one * 10 + free;
+        System.out.println("Число полученное перестановкой цифр: " + four);
+        */
+
+        /*
+        //Integer16 Дано трехзначное число.
+        // Вывести число, полученное при перестановке цифр десятков и единиц исходного числа (например, 123 перейдетв 132).
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите трёхзначное число: ");
+        int number = in.nextInt();
+        int one = number / 100;
+        int two = number % 10;
+        int free = (number / 10)%10;
+        int four = one * 100 + two * 10 + free;
+        System.out.println("Число полученное перестановкой цифр: " + four);
+        */
+       
+        
+        
+    }
+    
+}
