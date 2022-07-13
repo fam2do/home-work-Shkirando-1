23c4b0f524640c0c098f5755730926735444eb79
fam2do <108361807+fam2do@users.noreply.github.com>
2022-07-13 12:41:58 +0300

    Add files via upload

---
+++ homework3.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class homework3 {
+
+    public static void main(String[] args) {
+        //Boolean17
+        //Äàíî öåëîå ïîëîæèòåëüíîå ÷èñëî.
+        // Ïğîâåğèòü èñòèííîñòü âûñêàçûâàíèÿ: «Äàííîå ÷èñëî ÿâëÿåòñÿ íå÷åòíûì òğåõçíà÷íûì».
+        /*
+        Scanner qw = new Scanner(System.in);
+        System.out.println("Ââåäèòå ÷èñëî:");
+        int num = qw.nextInt();
+        boolean chis = (num % 2 != 0 && num > 99 && num < 1000);
+        System.out.println("Äàííîå ÷èñëî ÿâëÿåòñÿ íå÷¸òíûå òğ¸õçíà÷íûì:" + chis);
+        */
+        //Boolean19
+        //Ïğîâåğèòü èñòèííîñòü âûñêàçûâàíèÿ: «Ñğåäè òğåõ äàííûõ öåëûõ
+        //÷èñåë åñòü õîòÿ áû îäíà ïàğà âçàèìíî ïğîòèâîïîëîæíûõ».
+        /*
+        Scanner on = new Scanner(System.in);
+        int a = on.nextInt();
+        int b = on.nextInt();
+        int c = on.nextInt();
+        boolean res = a == -b && a == -c && b == -c;
+        System.out.println(res);
+        */
+        //Boolean21?
+        //.Äàíî òğåõçíà÷íîå ÷èñëî. Ïğîâåğèòü èñòèííîñòü âûñêàçûâàíèÿ:
+        //«Öèôğû äàííîãî ÷èñëà îáğàçóşò âîçğàñòàşùóş ïîñëåäîâàòåëüíîñòü».
+        /*
+        Scanner qw = new Scanner(System.in);
+        System.out.println("Ââåäèòå òğ¸õçíà÷íîå ÷èñëî:");
+        int num = qw.nextInt();
+        System.out.println("Ââåäèòå âòîğîå òğ¸õçíà÷íîå ÷èñëî:");
+        int num2 = qw.nextInt();
+        boolean number = (num > 99 && num2 < 1000 && num < num2);
+        System.out.println("×èñëà èìåşò âîçğàñòàşùóş ïîñëåäîâàòåëüíîñòü:" + number);
+        */
+
+
+
+    }
+}
+
