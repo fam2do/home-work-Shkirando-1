1358acb282d789b2cd18c32f78e992c7d8b5606a
fam2do <108361807+fam2do@users.noreply.github.com>
2022-08-14 21:51:56 +0300

    Add files via upload

---
+++ Homeworkif-else.java
@@ -0,0 +1,74 @@
+
+
+package com.mycompany.homework1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author famidos
+ */
+public class Homework1 {
+
+    public static void main(String[] args) {
+         //If8◦
+        //. Даны два числа. Вывести вначале большее, а затем меньшее из них.
+        /*
+        Scanner qw = new Scanner(System.in);
+        int a = qw.nextInt();
+        int b = qw.nextInt();
+        if (a > b) {
+            System.out.println("Большее число:" + a);
+            System.out.println("Меньшее число:" + b);
+        }
+        if (a < b) {
+            System.out.println("Большее число:" + b);
+            System.out.println("Меньшее число:" + a);
+         */
+        //If10. Даны две переменные целого типа: A и B. Если их значения не равны,
+        //то присвоить каждой переменной сумму этих значений, а если равны,
+        //то присвоить переменным нулевые значения. Вывести новые значения
+        //переменных A и B
+        /*
+        Scanner qw = new Scanner(System.in);
+        int a = qw.nextInt();
+        int b = qw.nextInt();
+        if (a != b) {
+            a = (a + b) && (b + a);
+            System.out.println(a);
+        } else {
+            (a == b)
+            b = (a * 0) && (b * 0);
+            System.out.println(b);
+            */
+        //If13. Даны три числа. Найти среднее из них (то есть число, расположенное
+        //между наименьшим и наибольшим)
+        /*
+        Scanner qw = new Scanner(System.in);
+        System.out.println("Введите число a: ");
+        int a = qw.nextInt();
+        System.out.println("Введите число b: ");
+        int b = qw.nextInt();
+        System.out.println("Введите число c: ");
+        int c = qw.nextInt();
+        if (b<c && a<c)System.out.println(a);
+        if (a<b && b<c)System.out.println(a);
+        if (a<c && c<b)System.out.println(a);
+        */
+        //If20. На числовой оси расположены три точки: A, B, C. Определить, какая из
+        //двух последних точек (B или C) расположена ближе к A, и вывести эту точку и ее расстояние от точки A.
+        /*
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите координаты A, B, C: ");
+        int A = in.nextInt();
+        int B = in.nextInt();
+        int C = in.nextInt();
+        if ((A - B) < (A - C)){
+            System.out.print("Точка B ближе к точке А" + " " +  "\n"+ "Расстояние от точки B до точки А:"  + " " + (A + B));
+        }else{
+            System.out.println("Точка C ближе к точке A" + " " +  "\n"+ "Расстояние от точки C до точки А:" + " " + (A + C));
+        */
+        }
+    }
+
+  
