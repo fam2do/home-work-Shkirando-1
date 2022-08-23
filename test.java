e022b754b4267675410203deb251af3afa277443
fam2do <108361807+fam2do@users.noreply.github.com>
2022-08-23 20:01:38 +0300

    Add files via upload

---
+++ test.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Вариант 1
+        //Задание 1
+            /*
+            int[] array = new int[10];
+            int p = 1;
+            for (int i = 0; i < array.length; i++) {
+                array[i] = (int) (Math.random() * 20);
+                p = p * array[i];
+            }
+            System.out.println(p);
+           */
+
+        // Задание 4
+        /*int sum = 0;
+        int[] massive = {-10, -9, -8, -7, -6, -5, -4, -3, -2, -1 , 0, 1, 1, 2, 3, 4, 5, 6, 7, 10};
+        //Сума положительных элементов массива
+        for (int i = 0; i < massive.length; i++) {
+            if (massive[i] > 0) {
+                sum = sum + massive[i];
+            }
+        }
+        System.out.println("Сума положительных элементов массива = "+sum);
+        */
+
+
+         // Задание 3
+        /*
+        int[] arr = new int[16];
+        int kolvo = 0;
+        Random Gen = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 3 == 0) {
+                System.out.print(kolvo + " " + i);
+            }else if
+                 (i % 5 == 0)
+                    System.out.println(kolvo + " " + i);
+            */
+
+            }
+        }
+
